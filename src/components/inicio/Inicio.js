2abb58d360006479084f62778297b2512ed43f99
Aaron Peña <l.aarongalvan@gmail.com>
2023-05-30 18:56:44 -0500

    Aaron

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -7,7 +7,7 @@ export const Inicio = () => {
     <div className={estilos.container}>
         <div>
             <p>
-                Descripción de la barbería hola como estas 
+                Descripción de la barbería 
             </p>
         </div>        
         <img src={imagen} className={estilos.imagen} alt="Logo de la barbería" />
