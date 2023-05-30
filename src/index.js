276fc6a8d78acbe7ce9b401fe80745a13a62766b
Juan Camilo Rubio <rubejuca@gmail.com>
2023-05-29 23:23:05 -0500

    Se agrego inicio y about

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Inicio } from './components/inicio/Inicio';
+import { About } from './components/about/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Inicio/>
+    <About/>
   </React.StrictMode>
 );
 
