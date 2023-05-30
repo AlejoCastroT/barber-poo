d1edb2fa6f92ca498fc3fcdfcce01b0f908fef1d
Juan Camilo Rubio <rubejuca@gmail.com>
2023-05-30 16:37:40 -0500

    camilor

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -7,7 +7,7 @@ export const Inicio = () => {
     <div className={estilos.container}>
         <div>
             <p>
-                Descripción de la barbería
+                Descripción de la barbería hola como estas 
             </p>
         </div>        
         <img src={imagen} className={estilos.imagen} alt="Logo de la barbería" />
