ac620e359cfcd3ddeb3f0934e9ed493f08234cbf
Juan Camilo Rubio <rubejuca@gmail.com>
2023-05-30 20:05:49 -0500

    cambios en el about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import estilos from './About.module.css'
+import imagen from '../../assets/sillaAbout.jpg'
 
 export const About = () => {
   return (
     <div class={estilos.container}>
-      <h1>ABOUT</h1>
+
+      <div className={estilos.content}>
+      <h1 class={estilos.content}>
+        ABOUT
+      </h1>
+      <p class={estilos.parrafo}>
+        loren ghghfhyjyerr jrjyrjyr h frmrmrr
+      </p>
+      </div>
+      <img className={estilos.imagen} src={imagen}>
+      </img>
     </div>
   )
 }
