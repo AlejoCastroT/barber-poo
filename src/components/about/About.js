61a14a3f0017ddce61be43847655cde87bb6a02d
Juan Camilo Rubio <rubejuca@gmail.com>
2023-06-12 18:58:37 -0500

    Cambios en about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,15 +7,16 @@ export const About = () => {
     <div class={estilos.container}>
       <div className={estilos.content}>
       <h1 class={estilos.content}>
-        ABOUT
+        ABOUT US
       </h1>
       <p class={estilos.parrafo}>
-      En [nombre de la barbería], nos apasiona ofrecer a nuestros clientes una experiencia de barbería excepcional.
+      En Old Style, nos apasiona ofrecer a nuestros clientes una experiencia de barbería excepcional.
       Nuestro talentoso equipo de barberos combina técnicas clásicas y modernas para brindarte cortes de cabello y estilos únicos.
       Nos enorgullece crear un ambiente acogedor donde puedas relajarte y disfrutar de un tiempo para ti. Utilizamos productos de alta calidad para cuidar tu cabello y piel,
-      y nos aseguramos de que cada visita sea personalizada. Ven a [nombre de la barbería] y descubre lo que ofrecemos. 
-      ¡Estamos ansiosos por conocerte y brindarte un servicio de primera clase!"
+      y nos aseguramos de que cada visita sea personalizada. Ven a Old Style y descubre lo que ofrecemos. 
+      ¡Estamos ansiosos por conocerte y brindarte un servicio de primera clase!
       </p>
+      <button class={estilos.button}>let's talk</button>
       </div>
       <img className={estilos.imagen} src={imagen} alt='silla'>
       </img>
